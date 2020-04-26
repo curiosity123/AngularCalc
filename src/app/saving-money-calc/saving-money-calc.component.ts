ceb4680da90082f4489cacdaa6d2538e38fd043b
Łukasz Adach <lukasz.adach7@gmail.com>
2020-04-26 13:40:18 +0200

    correction in googlechart

--- src/app/saving-money-calc/saving-money-calc.component.ts
+++ src/app/saving-money-calc/saving-money-calc.component.ts
@@ -6,34 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./saving-money-calc.component.scss']
 })
 export class SavingMoneyCalcComponent implements OnInit {
- 
- 
- myTitle = "test";
+
+
+ myTitle = 'When you will be able to buy product';
   myData = [
-    ["Jan",  7.0, -0.2, -0.9, 3.9],
-    ["Feb",  6.9, 0.8, 0.6, 4.2],
-    ["Mar",  9.5,  5.7, 3.5, 5.7],
-    ["Apr",  14.5, 11.3, 8.4, 8.5],
-    ["May",  18.2, 17.0, 13.5, 11.9],
-    ["Jun",  21.5, 22.0, 17.0, 15.2],
-    ["Jul",  25.2, 24.8, 18.6, 17.0],
-    ["Aug",  26.5, 24.1, 17.9, 16.6],
-    ["Sep",  23.3, 20.1, 14.3, 14.2],
-    ["Oct",  18.3, 14.1, 9.0, 10.3],
-    ["Nov",  13.9,  8.6, 3.9, 6.6],
-    ["Dec",  9.6,  2.5,  1.0, 4.8]
+    ['Jan',  7.0, -0.2],
+    ['Feb',  6.9, 0.8],
+    ['Mar',  9.5,  5.7],
+    ['Apr',  14.5, 11.3],
+    ['May',  18.2, 17.0],
+    ['Jun',  21.5, 22.0],
+    ['Jul',  25.2, 24.8],
+    ['Aug',  26.5, 24.1],
+    ['Sep',  23.3, 20.1],
+    ['Oct',  18.3, 14.1],
+    ['Nov',  13.9,  8.6],
+    ['Dec',  9.6,  2.5]
   ];
-
-  chartType =  'LineChart';
-
+  columnNames = ["Tokyo", "Your money","Product price"];
   options = {   
-    hAxis: {
-       title: 'Months'
-    },
-    vAxis:{
-       title: 'Price'
-    },
- };
+     hAxis: {
+        title: 'Month'
+     },
+     vAxis:{
+        title: 'Temperature'
+     },
+  };
+  chartType =  'LineChart';
+  width = 1500;
+  height = 400;
+ 
 
 
   constructor() { }
